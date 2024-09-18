6b652b7c30e7139598c6993976e1892830fc4182
Mirage <clementbobin21@gmail.com>
2024-09-18 08:24:59 +0200

    build: :white_check_mark: jest+playwright+i18

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,15 @@
-import NextAuth from 'next-auth';
-import { authConfig } from './auth.config';
- 
-export default NextAuth(authConfig).auth;
- 
+import { createI18nMiddleware } from 'next-international/middleware';
+import type { NextRequest } from 'next/server';
+
+const I18nMiddleware = createI18nMiddleware({
+  locales: ['en', 'fr'],
+  defaultLocale: 'en',
+});
+
+export function middleware(request: NextRequest) {
+  return I18nMiddleware(request);
+}
+
 export const config = {
-  // https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
-  matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
+  matcher: ['/((?!api|static|.*\\..*|_next|favicon.ico|robots.txt).*)'],
 };
